340a20580c5787941b5caba4c798eac435342654
LingMyat223 <lingmyataung@outlook.com>
2022-12-21 10:48:16 +0630

    vue cli

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,96 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row pt-4">
+      <div class="col-md-5 col-12 mb-3">
+        <div class="card shadow">
+          <div class="card-header">
+            <h3>{{ title1 }}</h3>
+          </div>
+          <div class="card-body">
+            <div class="input-group">
+              <input type="text" v-model="inputTask" class="form-control" />
+              <button
+                @click="addTodo"
+                class="btn btn-secondary"
+                :disabled="inputTask === ''"
+              >
+                Add+
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-7 col-12">
+        <div class="card shadow">
+          <div class="card-header">
+            <h3>{{ title2 }}</h3>
+          </div>
+          <div class="card-body">
+            <div class="">
+              <i
+                ><b>{{ text }}</b></i
+              >
+            </div>
+            <div v-for="(todo, index) in todos" :key="index">
+              <div class="d-flex gap-1">
+                <input type="checkbox" v-model="todo.done" id="" />
+                <span :class="{ done: todo.done }" class="d-inline-block"
+                  ><i>
+                    {{ todo.task }}
+                  </i></span
+                >
+                <button @click="deleteTodo(index)" class="btn btn-sm btn-danger" :disabled="!todo.done">
+                  <i class="fa-solid fa-xmark"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: () => ({
+    title1: "Add Your Tasks",
+    title2: "My Tasks",
+    inputTask: "",
+    todos: [],
+  }),
+  methods: {
+    addTodo() {
+      this.todos.push({
+        task: this.inputTask,
+        done: false,
+      });
+      this.setDefault();
+    },
+    deleteTodo(index){
+      this.todos.splice(index,1);
+    },
+    setDefault() {
+      this.inputTask = "";
+    },
+  },
+  computed: {
+    text() {
+      if (this.todos.length == 0) {
+        return "There is no task here";
+      }
+      return "";
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  user-select: none;
+}
+.done {
+  text-decoration: line-through;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import "bootstrap/dist/css/bootstrap.min.css"
 createApp(App).mount('#app')
